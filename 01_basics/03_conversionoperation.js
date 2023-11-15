d9e56e8bd982fef4a3791f8976551081f73f25e8
Shabuddin86 <142631189+Shabuddin86@users.noreply.github.com>
2023-11-15 22:18:27 +0000

    not complete

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+let score = false
+
+//console.log( typeof score);
+//console.log( typeof (score));
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+let isLoggedIn = "hithddjjdjdj"
+let booleanIsLoggedIn = Boolean(isLoggedIn )
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
