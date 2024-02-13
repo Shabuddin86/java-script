494bf20c84427a06551fbcf9b6af9b949d8d2aa3
Shabuddin86 <142631189+Shabuddin86@users.noreply.github.com>
2024-02-13 23:08:54 +0000

    data type summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+// JavaScript is a dynamically typed language
+
+// primitive
+
+// string.number.bigint.boolean.undefined.symbol.null.
+
+const score = 100
+
+const isloggedIn = false
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+// const bignumber = 111111111111166666455678999999999999999n
+
+
+
+// reference (non primitive)
+//  Object , Array, finction
+
+const heros = [ "hero alom" ,"ananta jalil", "zayed khan"];
+
+ {name:"shabuddin"
+age: 22
+}
+
+const myfunction = function  ( ) {console.log( "hello wrold");}
+    
+
+console.log( typeof score);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
