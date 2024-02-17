5ff4f546ae5e362f8003007fe50b831e48f07a18
Shabuddin86 <142631189+Shabuddin86@users.noreply.github.com>
2024-02-17 23:13:52 +0000

    play with string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "shabuddin"
+const repocount = 69
+
+// console.log( name + "  69" + " value");
+
+
+// console.log( ` hello my name is ${ name } and my repocount is ${repocount} `)
+
+const gameName = new String("shabuddin-islam");
+
+
+// console.log(gamename.__proto__);
+
+// console.log(gamename.toUpperCase());
+
+// console.log(gamename.indexOf('b'));
+
+// const newstring = gamename.substring ( 0, 4)
+
+// console.log( newstring);
+
+const anotherstring = gameName.slice(-9, 4)
+console.log(anotherstring)
+
+const newstringone = "  shabuddin   "
+
+console.log( newstringone);
+console.log( newstringone.trim());
+
+const url = "https://shabuddin.com/shabuddin%20islam"
+
+console.log(url.replace("%20" ,"-"));
+console.log(url.includes ('jony'));
+
+console.log(gameName.split( '-'));
