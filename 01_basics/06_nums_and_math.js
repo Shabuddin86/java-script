75cbc0eb129bf3b5ededb9ce3f90566cd5713f3d
Shabuddin86 <142631189+Shabuddin86@users.noreply.github.com>
2024-02-20 03:34:05 +0000

    2nd commit

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -28,3 +28,12 @@ console.log(Math.max(4,6,7,8));
 
 
 console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+
+const max = 20
+
+console.log(Math.floor(Math.random() *(max - min + 1)) + min)
+
+console.log(Math.floor(Math.random( ) * (max -min + 1) )+min)
