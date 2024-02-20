d60f84b70b9a80cf7eec70ff9938905a62cc2472
Shabuddin86 <142631189+Shabuddin86@users.noreply.github.com>
2024-02-20 03:15:12 +0000

    frsit commit

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score);
+
+const balance =  new Number (100)
+
+// console.log(balance );
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+ const othernumber = 11232.456799
+
+// console.log(othernumber.toPrecision(3));
+
+const hundeds = 1000000
+// console.log(hundeds.toLocaleString('en-IN'));
+
+// ************************math***********************
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(6.5));
+console.log(Math.floor(5.10));
+
+console.log(Math.min(4,6,7,8));
+console.log(Math.max(4,6,7,8));
+
+
+console.log(Math.random());
